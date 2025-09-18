2bd2da313058e27f292f699453a0fabed3cc3fa1
antoniamaria25 <mariaantoniavelosop@gmail.com>
2025-09-18 07:43:46 -0300

    up

--- gameplay/src/screens/index.tsx
+++ gameplay/src/screens/index.tsx
@@ -1,5 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {stylesSingIn} from './style';
+
 
 export  function SingIn() {
   return (
