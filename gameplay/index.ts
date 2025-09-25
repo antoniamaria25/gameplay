e4c6067d8e39139f5bebd6a907e37312935c6884
antoniamaria25 <mariaantoniavelosop@gmail.com>
2025-09-25 07:45:28 -0300

    up

--- gameplay/index.ts
+++ gameplay/index.ts
@@ -1,7 +1,17 @@
-import { registerRootComponent } from 'expo';
+import { StatusBar } from 'expo-status-bar';
+import { Text, View } from 'react-native';
+import {stylesSingIn} from './style';
+import { TextInput } from 'react-native';
 
-import App from './App';
-
+export  function SignIn() {
+  return (
+    <View style={stylesSingIn.container}>
+      <Text>digite alguma coisa</Text>
+      <TextInput style={{width:200,height:50,borderwight:2}}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
